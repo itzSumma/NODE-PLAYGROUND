b8cbfd7e7554a3d8ac0cad75487fdaf2640339e1
itzSumma <itzSumma11@gmail.com>
2026-05-09 09:16:55 +0600

    Create Express server with home, users and service routes

---
+++ basic-express/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from express learning World!')
+})
+// Another route
+
+app.get("/users",(req,res)=>{
+    res.send("Users route here")});
+
+
+    /*
+  *Nodemon installed globally:
+  *npm install -g nodemon
+
+  *Why?
+ * - Automatically server restart kore
+  *- Every change er por abar node run korte hoy na
+ * - Development faster hoy
+*/
+
+//Another route
+app.get("/service",(req,res)=>{
+    res.send("Service route here")
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
